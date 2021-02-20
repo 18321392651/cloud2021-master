30ccc7bfdd4357fbc626ad756c3ea710571829ec
ddw <497965015@qq.com>
2021-02-20 17:09:24 +0800

    ddw1

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/myrule/MySelfRule.java
@@ -0,0 +1,15 @@
+package com.atguigu.myrule;
+
+import com.netflix.loadbalancer.BestAvailableRule;
+import com.netflix.loadbalancer.IRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MySelfRule {
+
+    @Bean
+    public IRule myRule(){
+        return new BestAvailableRule();//定义为随机
+    }
+}
